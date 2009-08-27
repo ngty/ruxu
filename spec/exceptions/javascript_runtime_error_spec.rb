0c456ed75e481a58c1579f99a0eb8b4c803fc925
NgTzeYang <ngty77@gmail.com>
2009-08-27 13:49:11 +0800

    Misc tidy up.

--- spec/exceptions/javascript_runtime_error_spec.rb
+++ spec/exceptions/javascript_runtime_error_spec.rb
@@ -4,22 +4,20 @@ describe Ruxu::JavascriptRuntimeError do
 
   it_should_behave_like 'standard setup'
 
+  def error
+    Ruxu::JavascriptRuntimeError
+  end
+
   it 'should not be raised for valid javascript' do
-    lambda {
-      runner.send('var xyz = 123')
-    }.should_not raise_error(Ruxu::JavascriptRuntimeError)
+    lambda { runner.send('var xyz = 123') }.should_not raise_error(error)
   end
 
   it 'should be raised upon javascript syntax error' do
-    lambda {
-      runner.send('function() {')
-    }.should raise_error(Ruxu::JavascriptRuntimeError, /SyntaxError:/)
+    lambda { runner.send('function() {') }.should raise_error(error, /SyntaxError:/)
   end
 
   it 'should be raised upon javascript reference error' do
-    lambda {
-      runner.send('invalid.test()')
-    }.should raise_error(Ruxu::JavascriptRuntimeError, /ReferenceError:/)
+    lambda { runner.send('invalid.test()') }.should raise_error(error, /ReferenceError:/)
   end
 
 end
