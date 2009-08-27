e628f8d7b5fbed9096dfeb3a49c6c32b784ae194
NgTzeYang <ngty77@gmail.com>
2009-08-27 21:29:42 +0800

    Added support for throwing CannotOpenDocumentError when opening a page.

--- lib/ruxu/runner.rb
+++ lib/ruxu/runner.rb
@@ -4,12 +4,20 @@ module Ruxu
 
     extend Forwardable
 
-    def_delegators :@connection, :send, :read, :close
+    def_delegators :@connection, :send, :read, :close, :close_all
 
     def initialize(options={})
       @server = Server.new(options)
       @connection = Connection.new(options.merge(:server => @server))
       @connection.start
+    end
+
+    def open(location)
+      send(%\ruxu.open_page("#{location}")\)
+      sleep 0.5
+      if send('ruxu.page.document.title') =~ /Problem loading page/
+        raise CannotOpenDocumentError
+      end
     end
 
   end
